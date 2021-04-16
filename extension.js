b76a6add958213f7c46e74fa65cb0e7715d694ba
AmirRezaDalir <dalirnet@gmail.com>
2021-04-16 19:46:36 +0430

    start chunk script

--- extension.js
+++ extension.js
@@ -1,36 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const order = [
+    'el',
+    'name',
+    'components',
+    'filters',
+    'mixins',
+    'layout',
+    'props',
+    'asyncData',
+    'data',
+    'fetch',
+    'head',
+    'computed',
+    'watch',
+    'beforeCreate',
+    'created',
+    'beforeMount',
+    'mounted',
+    'beforeDestroy',
+    'methods',
+    'fetchOnServer',
+]
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vue-sort" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vue-sort.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Vue sort!');
-	});
-
-	context.subscriptions.push(disposable);
+const sort = () => {
+    const textEditor = vscode.window.activeTextEditor
+    if (textEditor) {
+        let chunk = {
+            import: {
+                global: {
+                    lib: [],
+                    mixins: [],
+                    components: [],
+                },
+                local: {
+                    lib: [],
+                    mixins: [],
+                    components: [],
+                },
+            },
+            module: {
+                el: [],
+                name: [],
+                components: [],
+                filters: [],
+                mixins: [],
+                layout: [],
+                props: [],
+                data: [],
+                fetch: [],
+                head: [],
+                computed: [],
+                watch: [],
+                beforeCreate: [],
+                created: [],
+                beforeMount: [],
+                mounted: [],
+                beforeDestroy: [],
+                methods: [],
+                fetchOnServer: [],
+            },
+        }
+        let pointerOnScriptBlock = false
+        for (let i = 0; i < textEditor.document.lineCount; i++) {
+            let { text, lineNumber } = textEditor.document.lineAt(i)
+            if (pointerOnScriptBlock && text.match(/<\/script.*>/i)) {
+                pointerOnScriptBlock = false
+            }
+            if (pointerOnScriptBlock) {
+                // chunk.push({ lineNumber, text })
+            }
+            if (!pointerOnScriptBlock && text.match(/<script.*>/i)) {
+                pointerOnScriptBlock = true
+            }
+        }
+        console.log(chunk)
+    } else {
+        vscode.window.showErrorMessage('"RTL Markdown" cannot be installed!')
+    }
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-
 module.exports = {
-	activate,
-	deactivate
+    activate: (context) => {
+        context.subscriptions.push([vscode.commands.registerCommand('vue-sort.sort', () => sort())])
+    },
+    deactivate: () => {},
 }
