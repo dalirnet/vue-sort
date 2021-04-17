bf2964f9fd0273e20daaf9d066080356df3bc959
AmirRezaDalir <dalirnet@gmail.com>
2021-04-17 19:25:18 +0430

    fix

--- extension.js
+++ extension.js
@@ -149,6 +149,28 @@ const sortModule = (lines) => {
         }
     ).keep
 
+    if (chunk.hasOwnProperty('components')) {
+        if (chunk.components.length > 2) {
+            chunk.components = [chunk.components[0], ...chunk.components.slice(1, -1).sort(), chunk.components[chunk.components.length - 1]]
+        }
+    }
+
+    if (chunk.hasOwnProperty('mixins')) {
+        if (chunk.mixins.length > 2) {
+            chunk.mixins = [chunk.mixins[0], ...chunk.mixins.slice(1, -1).sort(), chunk.mixins[chunk.mixins.length - 1]]
+        } else if (chunk.mixins.length == 1) {
+            const match = chunk.mixins[0].match(/^(.*\[)(.*)(\].*,)$/)
+            if (match) {
+                const sort = match[2]
+                    .split(',')
+                    .map((e) => e.trim())
+                    .sort()
+                    .join(', ')
+                chunk.mixins = [match[1] + sort + match[3]]
+            }
+        }
+    }
+
     return vscode.window.activeTextEditor.edit((builder) => {
         builder.replace(
             new vscode.Range(range.startLine, range.startCharacter, range.endLine, range.endCharacter),
@@ -172,22 +194,43 @@ const sort = () => {
             return Promise.resolve(scriptBlock())
         })
         .then((script) => {
-            return Promise.allSettled([
-                // import
-                // script.import.length ? sortImport(script.import) : Promise.resolve(),
-                // module
-                script.module.length ? sortModule(script.module) : Promise.resolve(),
-            ])
+            return new Promise((resolve, reject) => {
+                if (script.import.length) {
+                    sortImport(script.import)
+                        .then(() => {
+                            resolve(script)
+                        })
+                        .catch((e) => {
+                            reject(e)
+                        })
+                } else {
+                    resolve(script)
+                }
+            })
+        })
+        .then((script) => {
+            return new Promise((resolve, reject) => {
+                if (script.module.length) {
+                    sortModule(script.module)
+                        .then(() => {
+                            resolve()
+                        })
+                        .catch((e) => {
+                            reject(e)
+                        })
+                } else {
+                    resolve()
+                }
+            })
         })
         .then(() => {
-            return Promise.resolve()
-            // return vscode.commands.executeCommand('editor.action.formatDocument')
+            return vscode.commands.executeCommand('editor.action.formatDocument')
         })
         .then(() => {
-            console.log('order')
+            vscode.window.setStatusBarMessage('Sorted vue component', 3000)
         })
         .catch(({ message }) => {
-            console.log(message)
+            vscode.window.showErrorMessage(message)
         })
 }
 
